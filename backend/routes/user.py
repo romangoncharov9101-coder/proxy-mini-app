19aef054ab6649eebac099e70d5912e8b787a138
Roman <romangoncharov9101@gmail.com>
2026-05-08 17:24:30 +0300

    fix show user`s proxy

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -288,7 +288,7 @@ async def get_my_proxies(
 
     result = await get_proxy_page(
         db,
-        owner_id=current_user.id,
+        key_id=current_user.api_key_id,
         last_id=last_id,
         limit=limit,
         search=search,
